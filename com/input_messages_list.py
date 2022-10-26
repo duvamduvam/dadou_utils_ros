02026ff50351bf270931087a8f8950aeb8562877
duvamduvam <david@mail.duvam.net>
2022-10-27 00:14:55 +0200

    udpate

--- com/input_messages_list.py
+++ com/input_messages_list.py
@@ -14,6 +14,6 @@ class InputMessagesList(metaclass=SingletonMeta):
     def pop_msg(self):
         msg = None
         if len(self.messages) > 0:
-            msg = json.loads(self.messages[0])
+            msg = self.messages[0]
             self.messages.pop()
         return msg
